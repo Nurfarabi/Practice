0068f3f6c5c91a4dbe6e368f78657a5b88ef87e2
Moder <moder@gmail.com>
2026-03-06 23:20:42 +0500

    Regex examples of functions, quantifiers, metacharacters, sets and special sequences

---
+++ Practice_5/Regex_exercises/findall.py
@@ -0,0 +1,9 @@
+import re
+
+txt = "The rain in Spain"
+x = re.findall("ai", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.findall("Portugal", txt)
+print(x)
---
+++ Practice_5/Regex_exercises/metacharacters_quantifiers.py
@@ -0,0 +1,71 @@
+import re
+"[] - A set of characters"
+txt = "The rain in Spain"
+#Find all lower case characters alphabetically between "a" and "m":
+x = re.findall("[a-m]", txt)
+print(x)
+
+"""\ - Signals a special sequence (can also be used to escape special characters)"""
+txt = "That will be 59 dollars"
+#Find all digit characters:
+x = re.findall("\d", txt)
+print(x)
+
+". - Any character (except newline character)"
+txt = "hello planet"
+#Search for a sequence that starts with "he", followed by two (any) characters, and an "o":
+x = re.findall("he..o", txt)
+print(x)
+
+"^ 	Starts with"
+txt = "hello planet"
+#Check if the string starts with 'hello':
+x = re.findall("^hello", txt)
+if x:
+  print("Yes, the string starts with 'hello'")
+else:
+  print("No match")
+
+"$ ends with"
+txt = "hello planet"
+#Check if the string ends with 'planet':
+x = re.findall("planet$", txt)
+if x:
+  print("Yes, the string ends with 'planet'")
+else:
+  print("No match")
+
+"* Zero or more occurrences"
+txt = "hello planet"
+#Search for a sequence that starts with "he", followed by 0 or more  (any) characters, and an "o":
+x = re.findall("he.*o", txt)
+print(x)
+
+"+ 1 or more occurences"
+txt = "hello planet"
+#Search for a sequence that starts with "he", followed by 1 or more  (any) characters, and an "o":
+x = re.findall("he.+o", txt)
+print(x)
+
+"? zero or one occurences"
+txt = "hello planet"
+#Search for a sequence that starts with "he", followed by 0 or 1  (any) character, and an "o":
+x = re.findall("he.?o", txt)
+print(x)
+#This time we got no match, because there were not zero, not one, but two characters between "he" and the "o"
+
+"{} Exactly the specified number of occurrences"
+txt = "hello planet"
+#Search for a sequence that starts with "he", followed excactly 2 (any) characters, and an "o":
+x = re.findall("he.{2}o", txt)
+print(x)
+
+"| either or"
+txt = "The rain in Spain falls mainly in the plain!"
+#Check if the string contains either "falls" or "stays":
+x = re.findall("falls|stays", txt)
+print(x)
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
